685ac96388d6f7f52e16591c1c15f2dd57daeb68
fmael <mael.fath@ynov.com>
2024-03-28 15:05:00 +0100

    Added web-app

---
+++ web-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./index.css";
+
+function App() {
+
+
+
+
+  return (
+    <div>
+      <header>
+        <h1>Data App</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
