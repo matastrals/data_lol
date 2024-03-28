25803fbbfb21e3337e4218b6b6e1b8e168bd1758
mmattis <mattis.milochau@ynov.com>
2024-03-28 16:09:05 +0100

    2 function finished

--- server-app/back.py
+++ server-app/back.py
@@ -1,12 +1,31 @@
 import pandas as pd
 import flask as fk
 
-app = fk(__name__, static_url_path='/static')
-
+app = fk.Flask(__name__, static_url_path='/static')
 
 df = pd.read_csv("lol.csv", index_col=0, sep=";")
-# print(df.head(10))
-
-@app.route("/")
 
 
+@app.route("/role")
+def pourcentOfRole():
+    role_counts = df['Role'].value_counts()
+    total_entries = len(df)
+    role_percentages = (role_counts / total_entries) * 100
+    role_percentages_rounded = role_percentages.round(1)
+    role_percentages_formatted = role_percentages_rounded.apply(lambda x: f"{x:.1f}%")
+    json_result = role_percentages_formatted.to_json(orient='index')
+    return json_result
+
+
+@app.route("/class")
+def class_percentages():
+    class_counts = df['Class'].value_counts()
+    total_entries = len(df)
+    class_percentages = (class_counts / total_entries) * 100
+    class_percentages_rounded = class_percentages.round(1)
+    class_percentages_formatted = class_percentages_rounded.apply(lambda x: f"{x:.1f}%")
+    json_result = class_percentages_formatted.to_json(orient='index')
+    return json_result
+
+if __name__ == "__main__":
+    app.run(debug=True)
