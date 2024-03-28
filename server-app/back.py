6ead6853a90a6b1613f2a11f0d90a2a6df757cfd
mmattis <mattis.milochau@ynov.com>
2024-03-28 15:45:41 +0100

    begin flask

--- server-app/back.py
+++ server-app/back.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import flask as fk
 
+app = fk(__name__, static_url_path='/static')
+
+
+df = pd.read_csv("lol.csv", index_col=0, sep=";")
+# print(df.head(10))
+
+@app.route("/")
+
+
